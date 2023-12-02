999c7a1a2911c50b1819d3a80d6bd558266f0716
Erkan Uretener <erkan808987@gmail.com>
2023-12-02 15:25:32 -0500

    Cleaned up code -> fixed get query method -> built generic struct refactoring methods to parse get queries

--- main.go
+++ main.go
@@ -14,21 +14,8 @@ import (
 
 func main() {
 
-	// sources := []int{Mod.AB, Mod.SNS, Mod.SM, Mod.FOL}
-
-	// //1 -> Source, 2 -> product, 3 -> channel
-	// var sourceData map[int][]int = make(map[int][]int)
-
-	// for i := 0; i < len(sources); i++ {
-	// 	sourceData[sources[i]] = make([]int, 100)
-	// }
-
-	// for i := 0; i < len(sources); i++ {
-	// 	sourceData[sources[i]][0] = len(Lib.GetDataList(Mod.GET_SEARCH_VARIANTS_PATH, model.GetSearchInventoryVariants{FilterSourceId: 48226}))
-	// 	// sourceData[sources[i]][0] = len(Lib.GetDataList(Mod.GET_PRODUCT_VARIANTS_PATH, model.GetProductVariant{SourceId: sources[i]}))
-	// 	// sourceData[sources[i]][0] = len(Lib.GetDataList(Mod.GET_INVENTORY_VARIANTS_PATH, model.GetInventoryVariant{SourceId: sources[i]}))
-	// }
-
 	extent := Mod.QueryUrl(Mod.GetInventoryVariant{Page: 4})
-	pretty.Print(Lib.GetDataList(Mod.GET_INVENTORY_VARIANTS_PATH+extent, os.Getenv("FLX_AB_TOKEN")))
+	// pretty.Print(Lib.GetDataList(Mod.GET_INVENTORY_VARIANTS_PATH+extent, os.Getenv("FLX_AB_TOKEN")))
+	// pretty.Print(Lib.GetDataList(Mod.GET_INVENTORY_VARIANTS_PATH+extent, os.Getenv("FLX_SS_TOKEN")))
+	pretty.Print(Lib.GetDataList(Mod.GET_INVENTORY_VARIANTS_PATH+extent, os.Getenv("FLX_SM_TOKEN")))
 }
